d7070de2e2fe7106efb9ae328a9919982f8286fa
Erick José Fuentes Mejía <erickjosef94@outlook.com>
2021-08-14 17:36:14 -0600

    Primer commit de mi repositorio

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eri
+ */
+public class NewClass {
+  public static void main(String[] args) {
+Scanner entrada = new Scanner(System.in);
+double base, altura, area;
+System.out.println("Escriba la Base del rectanulo:");
+base = Double.parseDouble(entrada.next());
+System.out.println("Ingrese la Altura del rectangulo:");
+altura = Double.parseDouble(entrada.next());
+area = base * altura;
+System.out.println("El Area del rectangulo es:" + area);
+}
+    
+}
